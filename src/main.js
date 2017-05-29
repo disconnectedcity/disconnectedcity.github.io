03f34dd5296ed04df73cf8fb06ea53b90ac8891f
Antti Rautanen <antti.rautanen@reaktor.fi>
2017-05-29 10:45:45 +0300

    Remove everything

--- src/main.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  var isDevelopmentEnvironment = window.location.hostname === '127.0.0.1'
-  if (isDevelopmentEnvironment) {
-    document.write('<script src="http://127.0.0.1:35729/livereload.js?snipver=1"></script>')
-  }
-})()
--- tiny-lr.js
+++
@@ -1,3 +0,0 @@
-var tinylr = require('tiny-lr');
-var port = 35729;
-tinylr().listen(port);
